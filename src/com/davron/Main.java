bb45fe6ec4b0a47b85d161268f5d894cba5a78af
davronismoilov <ismoilovdavron16@gmail.com>
2022-06-22 09:22:41 +0500

    Initial commit

---
+++ src/com/davron/Main.java
@@ -0,0 +1,12 @@
+package com.davron;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static void  testimonies(){
+        System.out.println("dshbkdb");
+    }
+}
